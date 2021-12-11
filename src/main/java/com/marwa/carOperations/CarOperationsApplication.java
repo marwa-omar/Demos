09997f27b1e8ffafdcdd24c13a97cfb9f43239f6
marwa <marwaomar1996@gmail.com>
2021-12-11 17:49:38 +0200

    initial commit

---
+++ src/main/java/com/marwa/carOperations/CarOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.marwa.carOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marwa/carOperations/CarOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marwa.carOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
